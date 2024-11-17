22f631d8f1544298359738413411475229f26be6
Arman Mondal <arman.mondal.27012005@gmail.com>
2024-11-17 19:38:01 +0530

    modified:   pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title >Link Shortner by Arman Mondal</title>
+        <meta name="description" content="A simple link shortner" />
+        
+        </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
